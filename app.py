222a0fd69276ddc073ca3c5166ccf8b1b06ed229
Lauren To <laurenemilyto@gmail.com>
2021-11-18 17:19:49 -0500

    added lg model, pickle, app.py and heroku setup

---
+++ app.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+import tensorflow as tf
+import numpy as np
+import keras
+import os
+import time
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# SQLite for information
+import sqlite3
+
+# Define a flask app
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+def info():
+    conn = sqlite3.connect("models/crop.sqlite")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM crop")
+    rows = cursor.fetchall()
+    return rows
+
+    def leaf_predict(img_path):
+        # load image with target size
+    img = image.load_img(img_path, target_size=(256, 256))
+    # convert to array
+    img = image.img_to_array(img)
+    # normalize the array
+    img /= 255
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    # output = round(prediction[0], 2)
+    if prediction==0:
+        return render_template('index.html',
+                               prediction_text='Low chances of patient having diabetes'.format(prediction),
+                               )
+    else:
+        return render_template('index.html',
+                               prediction_text='High chances of patient having diabetes'.format(prediction),
+                              )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
