f64da1a855e9f18cb8868f39ca67cc3b24b01fe7
Yingying Li <yixuan40@gmail.com>
2021-11-20 13:47:08 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
 # coding=utf-8
-import tensorflow as tf
 import numpy as np
-import keras
 import os
 import time
 from flask import Flask, request, jsonify, render_template
 import joblib
 
-# SQLite for information
-import sqlite3
-
 # Define a flask app
 app = Flask(__name__)
-model = joblib.load('../models/rf_model.h5')
-
-# def info():
-#     conn = sqlite3.connect("models/crop.sqlite")
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT * FROM crop")
-#     rows = cursor.fetchall()
-#     return rows
-
-#     def leaf_predict(img_path):
-#         # load image with target size
-#     img = image.load_img(img_path, target_size=(256, 256))
-#     # convert to array
-#     img = image.img_to_array(img)
-#     # normalize the array
-#     img /= 255
+model = joblib.load('models/rf_model.h5')
 
 @app.route('/')
 def index():
