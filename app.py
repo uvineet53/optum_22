9beaf951e95f409b907d3a2385947dca51bfc5a1
Yingying Li <yixuan40@gmail.com>
2021-11-23 20:18:14 -0500

    modified app.py and index.html

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def predict():
         gender = int(request.form['gender'])
 
         prediction = model.predict([[age, time_in_hospital, number_of_procedures, number_of_medications, race, gender]])
-        output = round(prediction[0],2)
+        # output = round(prediction[0],2)
 
-        if output<0:
+        if prediction == 0:
             return render_template('index.html', prediction_text='Low chances of patient readmitted to hospital within 30 days.')
         else:
             return render_template('index.html', prediction_text='High chances of patient readmitted to hospital within 30 days')
